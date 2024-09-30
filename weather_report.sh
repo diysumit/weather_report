348e7cbef02011c4d83ca179c7f75edc3402f108
diysumit <diysumit@gmail.com>
2024-09-30 16:24:13 +0530

    cleaned up script, fixed the location

--- weather_report.sh
+++ weather_report.sh
@@ -3,10 +3,13 @@
 # using GNU grep
 brew install grep
 
-# exclude next line
-echo -e "Enter location name: "
+# exclude next line, remove from comment to make it interactive
+#echo -e "Enter location name: "
 
-read location
+#read location
+
+#using fixed value, comment it out when using interactive mode
+location=casablanca
 
 #using ?2 returns current and next day data
 curl -s wttr.in/$location?2 > latest_weather_report
@@ -17,9 +20,6 @@ today=$(cat "`pwd`/latest_weather_report" | head -n 7 | ggrep -Po '\+\d+' | tr -
 #getting tomorrows temp at noon
 forcasted=$(cat "`pwd`/latest_weather_report" | tail | ggrep -Po "\+\d+" | tr -d + | tail -n 3 | head -n 1)
 
-echo $today
-echo $forcasted
-
 echo "`date +"%Y %m %d"` $today $forcasted"
 
 if ! test -f ./weather_report; then
@@ -28,6 +28,11 @@ fi
 
 echo "`date +"%Y %m %d"` $today $forcasted" >> weather_report
 
+
+cat weather_report
+
+#printing complete data for next two days
+curl wttr.in/$location
 #adding this to crontab
 #crontab -r 
 #crontab -e
